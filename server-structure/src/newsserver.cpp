611641e8250c2b78f1663fa4288b89f75f9bc714
ChrisOlsson <chris.olsson123@gmail.com>
2012-03-19 22:30:32 +0100

    I have created the server class to implement, called newsserver. Not sure what Josef has done when creating the server-2.cpp class, not sure what it is for, but use the newsserver for now =P

---
+++ server-structure/src/newsserver.cpp
@@ -0,0 +1,28 @@
+/*
+ * newsserver.cpp
+ *
+ *  Created on: 19 mar 2012
+ *      Author: Chris
+ *
+ *  Description: This is the server we should implement...
+ */
+
+#include "server.h"
+#include "connection.h"
+#include "connectionclosedexception.h"
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+using client_server::Server;
+using client_server::Connection;
+using client_server::ConnectionClosedException;
+
+int main(int argc, char* argv[]){
+
+
+
+
+}
